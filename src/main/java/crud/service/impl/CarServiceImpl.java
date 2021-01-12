38ef748dd921941ea2f5379c741366a426c02b39
kerrender <agreed_note@hotmail.com>
2021-01-12 10:58:38 +0200

    intermediate

---
+++ src/main/java/crud/service/impl/CarServiceImpl.java
@@ -0,0 +1,32 @@
+package crud.service.impl;
+
+import crud.model.Car;
+import crud.service.abstraction.CarService;
+import java.util.Map;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public Car create(Car element) {
+        return null;
+    }
+
+    @Override
+    public Car get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Map<Long, Car> getAll() {
+        return null;
+    }
+
+    @Override
+    public Car update(Car element) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
