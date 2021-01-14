49727202f26a2c752f303a9c7a24c78a8674785d
Max Pochepets <agreed_note@hotmail.com>
2021-01-14 14:26:24 +0200

    Refactor:
    got all statements to try with resources closure
    added objects that failed to proceed to exception message
    deleted unused query
    changed util methods to non-static

--- src/main/java/crud/dao/impl/ManufacturerDaoJdBcImpl.java
+++ src/main/java/crud/dao/impl/ManufacturerDaoJdBcImpl.java
@@ -19,18 +19,17 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
         String query = "INSERT INTO manufacturers (name,country) VALUES (?,?)";
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement preparedStatement
-                    = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement
+                        = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             setUpdate(preparedStatement, manufacturer).executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 manufacturer.setId(resultSet.getObject(1, Long.class));
             }
-            preparedStatement.close();
             return manufacturer;
         } catch (SQLException throwables) {
-            throw new DataBaseException("Couldn't create manufacturer.",
+            throw new DataBaseException("Couldn't create manufacturer. " + manufacturer + " ",
                     throwables);
         }
     }
@@ -39,17 +38,16 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
     public Optional<Manufacturer> get(Long id) {
         String query = "SELECT id, name, country FROM manufacturers WHERE id = "
                 + id + " AND deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            Statement statement = connection.createStatement();
+        try (Connection connection = ConnectionUtil.getConnection();
+                Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(query);
             Manufacturer manufacturer = null;
             if (resultSet.next()) {
                 manufacturer = setManufacturer(resultSet);
             }
-            statement.close();
             return Optional.ofNullable(manufacturer);
         } catch (SQLException throwables) {
-            throw new DataBaseException("Couldn't get manufacturer by id.",
+            throw new DataBaseException("Couldn't get manufacturer by id " + id + " ",
                     throwables);
         }
     }
@@ -57,17 +55,17 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
     @Override
     public List<Manufacturer> getAll() {
         String query = "SELECT * FROM manufacturers WHERE deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection()) {
+        try (Connection connection = ConnectionUtil.getConnection();
+                Statement statement = connection.createStatement()) {
             List<Manufacturer> manufacturers = new ArrayList<>();
-            Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 manufacturers.add(setManufacturer(resultSet));
             }
-            statement.close();
             return manufacturers;
         } catch (SQLException throwables) {
-            throw new DataBaseException("Couldn't get a list of manufacturers.",
+            throw new DataBaseException("Couldn't get a list of manufacturers "
+                    + "from manufacturers table. ",
                     throwables);
         }
     }
@@ -76,16 +74,14 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
     public Manufacturer update(Manufacturer manufacturer) {
         String query = "UPDATE manufacturers SET name = ?, country = ?"
                 + " WHERE id = ? AND deleted = FALSE";
-        String getNewQuery = "SELECT * FROM manufacturers WHERE id = " + manufacturer.getId();
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement preparedStatement
-                    = setUpdate(connection.prepareStatement(query), manufacturer);
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement
+                        = setUpdate(connection.prepareStatement(query), manufacturer)) {
             preparedStatement.setLong(3, manufacturer.getId());
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return manufacturer;
         } catch (SQLException throwables) {
-            throw new DataBaseException("Couldn't update a specific manufacturer.",
+            throw new DataBaseException("Couldn't update a manufacturer " + manufacturer + " ",
                     throwables);
         }
     }
@@ -93,17 +89,17 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
     @Override
     public boolean delete(Long id) {
         String query = "UPDATE manufacturers SET deleted = TRUE WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setLong(1, id);
             return preparedStatement.executeUpdate() > 0;
         } catch (SQLException throwables) {
-            throw new DataBaseException("Couldn't delete a manufacturer by id.",
+            throw new DataBaseException("Couldn't delete a manufacturer by id " + id + " ",
                     throwables);
         }
     }
 
-    private static Manufacturer setManufacturer(ResultSet resultSet) throws SQLException {
+    private Manufacturer setManufacturer(ResultSet resultSet) throws SQLException {
         Long newId = resultSet.getObject("id", Long.class);
         String name = resultSet.getString("name");
         String country = resultSet.getString("country");
@@ -112,7 +108,7 @@ public class ManufacturerDaoJdBcImpl implements ManufacturerDao {
         return manufacturer;
     }
 
-    private static PreparedStatement setUpdate(PreparedStatement statement,
+    private PreparedStatement setUpdate(PreparedStatement statement,
                                                Manufacturer manufacturer) throws SQLException {
         statement.setString(1, manufacturer.getName());
         statement.setString(2, manufacturer.getCountry());
