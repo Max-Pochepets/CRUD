4f1c9836ab9463fbb55baa277f9bfe34d03329c4
Max Pochepets <agreed_note@hotmail.com>
2021-01-14 22:14:32 +0200

    first

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,3 +5,4 @@ CREATE TABLE `taxi`.`manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
