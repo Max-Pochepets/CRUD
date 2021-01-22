4bfd010a66667922f0b4b9ddcbc6a173ea1257a2
Max Pochepets <agreed_note@hotmail.com>
2021-01-22 12:10:29 +0200

    Fix: redundant check in Filter removed

--- src/main/java/crud/web/filters/AuthenticationFilter.java
+++ src/main/java/crud/web/filters/AuthenticationFilter.java
@@ -33,7 +33,7 @@ public class AuthenticationFilter implements Filter {
             return;
         }
         Long driverId = (Long) req.getSession().getAttribute(DRIVER_ID);
-        if (driverId == null || driverService.get(driverId) == null) {
+        if (driverId == null) {
             resp.sendRedirect("/login");
             return;
         }
